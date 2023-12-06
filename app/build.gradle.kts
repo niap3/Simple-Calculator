65a6d115188c7e3dbbd6d04ec07813a8107f3026
Edvin Joseph <edvinjoseph367@gmail.com>
2023-12-07 04:49:23 +0530

    Committing changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,12 @@
+
+
 plugins {
     id("com.android.application")
 }
 
 android {
     namespace = "com.example.simplecalculator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.simplecalculator"
@@ -34,7 +36,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
